dc7e22de134ef0ced48379148a1964bb442e7eb1
kali <s1nj0r0@protonmail.com>
2020-06-07 17:43:06 -0400

    typo in hashPassword

--- backend/src/controllers/md5/HashController.ts
+++ backend/src/controllers/md5/HashController.ts
@@ -17,14 +17,14 @@ export class HashesController {
   @Post("/login")
   public login(@BodyParams() body: ILogin): ICheckResponse {
     // https://crackstation.net/ - securepassword = b0439fae31f8cbba6294af86234d5a28
-    if (body.username === "admin" && this.hashPassowrd(body.password) === "b0439fae31f8cbba6294af86234d5a28")
+    if (body.username === "admin" && this.hashPassword(body.password) === "b0439fae31f8cbba6294af86234d5a28")
       return { success: true, flag: "c9d0ebbe-ced2-4a32-8e51-c1220a492cc0" };
     else
       return { success: false, flag: "" };
   }
 
 
-  private hashPassowrd(password: string): string {
+  private hashPassword(password: string): string {
     return crypto.createHash('md5').update(password).digest("hex");
   }
 }
