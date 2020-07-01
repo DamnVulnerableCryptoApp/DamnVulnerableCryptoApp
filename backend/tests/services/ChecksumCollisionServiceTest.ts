8d1b4d02f9959d9f275cfc0ab3dbcb3d54ce0f3f
Luis Fontes <luisfontes101@gmail.com>
2020-07-01 22:25:37 +0100

    changed public key line endings for checksums to produce same output in tests in linux systems

--- backend/tests/services/ChecksumCollisionServiceTest.ts
+++ backend/tests/services/ChecksumCollisionServiceTest.ts
@@ -12,12 +12,12 @@ describe("ChecksumCollisionService", () => {
 
   it("Should calculate md5 hash", async () => {
     const r = await ChecksumCollisionService.getMd5FileChecksum(__dirname + "/../../src/config/publickey.pem");
-    expect(r).to.be.equal("9618c9f82606cd2ab0e333d225cd48a2");
+    expect(r).to.be.equal("6e3abd9cef2163cb37ff4936a72df849");
   });
 
 
   it("Should calculate sha1 hash", async () => {
     const r = await ChecksumCollisionService.getSha1FileChecksum(__dirname + "/../../src/config/publickey.pem");
-    expect(r).to.be.equal("0d6f4912f5b2800cba5d79d5f036a1a79ee61303");
+    expect(r).to.be.equal("81ccb0bac342760799493de699407fc9d3803e96");
   });
 });
