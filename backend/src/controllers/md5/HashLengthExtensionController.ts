453b2bca34fd2f5964f482da442a69be385fe82c
Luis Fontes <luisfontes@protonmail.com>
2020-09-28 22:18:10 +0100

    removed debug lines

--- backend/src/controllers/md5/HashLengthExtensionController.ts
+++ backend/src/controllers/md5/HashLengthExtensionController.ts
@@ -23,8 +23,7 @@ export class HashLengthExtensionController {
     let tampered = false;
     // data = "56476870637942706379427164584E30494746756233526F5A5849675A57467A644756794947566E5A793467534746325A534235623355675A6D3931626D51676447686C625342686247772F\x80\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x06\x18asd"
     // signature = "1346d98335868e2da281eda2b48dd6954b91566f19f7a84bf24e22867f1a60a2"
-    console.log(data.data)
-    console.log(data.signature)
+
     console.log(HashLengthExtensionService.sign(data.data))
 
     if (HashLengthExtensionService.sign(data.data) === data.signature) {
