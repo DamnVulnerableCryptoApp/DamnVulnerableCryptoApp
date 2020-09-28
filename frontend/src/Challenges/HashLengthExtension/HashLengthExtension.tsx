0ba7daca75bb3b2aa1639672ec0adca060878e4b
Luis Fontes <luisfontes@protonmail.com>
2020-09-28 09:54:36 +0100

    added missing semicolon

--- frontend/src/Challenges/HashLengthExtension/HashLengthExtension.tsx
+++ frontend/src/Challenges/HashLengthExtension/HashLengthExtension.tsx
@@ -37,7 +37,7 @@ const HashLengthExtension = (props: IChallengeProps) => {
       }).catch(err => {
         layoutContext.setSnackErrorMessage("Some error message");
         layoutContext.setLoading(false);
-      })
+      });
     }).catch(err => {
       layoutContext.setSnackErrorMessage("Some error message");
       layoutContext.setLoading(false);
